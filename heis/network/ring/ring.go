fafc0e3aecae0073f4d203c73af9cedf6642b9da
Haakon Robinson <haakon.robinson@gmail.com>
2017-03-09 21:44:57 +0100

    ringnode almost done

--- heis/network/ring/ring.go
+++ heis/network/ring/ring.go
@@ -1,37 +1,72 @@
 package ring 
 
 import (
+	"net"
 	"os"
+	"fmt"
+	_"json"
 )
 
-func Node(incomingCh chan string, outgoingCh chan string, connUpdateCh chan [2]string, port int){
-	var initialised = false
-
-	incomingChan := make(chan string)
-	newIP := <-updateChannel
-	go resetPrevNode(incomingChan, TCPPortIn)
-	time.Sleep(time.Second)
-	updateNextNode(newIP+TCPPortIn)
-	
+func NextNode(outgoingCh chan string, updateCh chan string, port int){
+	initialised := false
+	nextAddr := ""
+	var conn net.Conn
 	
 	for {
 		select {
-			case msg := <-connUpdateCh:
-				switch(msg){
-				case
-				fmt.Println("Updated")
-				go resetPrevNode(incomingChan, TCPPortIn)
-				updateNextNode(msg)
-			case msg := <-incomingChan:
-				elevChannel<- msg
-				time.Sleep(time.Second)
-				nextNode.Write([]byte(msg)) 
-				// errorCheck
-				//go resetPrevNode(incomingChan, TCPPortIn)
+		case nextAddr = <-updateCh:
+			if initialised {
+				conn.Close()
+			}
+			IP, _ := net.ResolveTCPAddr("tcp",nextAddr)
+			conn, err = net.DialTCP("tcp", nil, IP)
+			if err == nil {
+				connUpdateCh<- "OK"
+				initialised = true
+			} else {
+				connUpdateCh<- "ERROR"
+				initialised = false	
+			}
+		case msg := <-outgoingCh:
+			if initialised {
+				_, err = conn.Write([]byte(msg))
+			} else if (err != nil) || (!initialised) {
+				outgoingCh<- "ERROR"
+			} else {
+				outgoingCh<- "OK"
+			}
 		}
 	}
 
 }
 
+func PrevNode(incomingCh chan string, updateCh chan string, port int){
+	initialised := false
+	//var prevAddr = ""
+	var conn net.Conn
+	var buf [1024]byte
+	
+	for {
+		if !initialised {
+			go func(){
+				ln, _ := net.ListenTCP("tcp", addr)
+				conn, err := ln.Accept()
+				if err != nil {
+					initialised = true
+				}
+			}
+		}
+		select {
+		case update := <-updateCh:
+			if update == "RESET" {
+				initialised = false
+			}
+		default:
+			n, _, _ := conn.ReadFrom(buf[0:])
+			msg := string(buf[:n])
+			
+		}
+	}
+}
 
-func NextNode(){}
+}
